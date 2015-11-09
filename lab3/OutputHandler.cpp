df4a536fc37a06f496e6ebce763f9733b562bf64
xiaoyuxqx <xiaoyuxqx@gmail.com>
2015-11-09 10:36:51 -0600

    save for Anqi's clone -- need to add LL(2) scanner

---
+++ lab3/OutputHandler.cpp
@@ -0,0 +1,127 @@
+﻿#include "stdafx.h"
+#include "OutputHandler.h"
+#include <windows.h>
+
+OutputHandler output_handler(std::cout);
+
+short OutputHandler::nline_user_used_ = 0;
+
+OutputHandler::OutputHandler(std::ostream& out) : 
+    out_(out), hint_zone_row_(0), prompt_x_(0), prompt_y_(0)
+{}
+
+short OutputHandler::GetWindowWidth() const {
+    short width = -1;
+    short height = -1;
+    GetWindowSize(width, height);
+    return width;
+}
+
+short OutputHandler::GetWindowHeight() const {
+    short width = -1;
+    short height = -1;
+    GetWindowSize(width, height);
+    return height;
+}
+
+short OutputHandler::GetPrintableZoneHeight() const {
+    short window_height = GetWindowHeight();
+    return window_height < nLinesBottomUsed + nLinesTopUsed ? 0 : window_height - nLinesBottomUsed - nLinesTopUsed;
+}
+
+int OutputHandler::GetWindowSize(short& width, short& height) {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int ret = GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    if (ret) {
+        width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    }
+    return ret;
+}
+
+int OutputHandler::GetCursorPos(short& x, short& y) {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int ret = GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    if (ret) {
+        x = csbi.dwCursorPosition.X;
+        y = csbi.dwCursorPosition.Y;
+    }
+    return ret;
+}
+
+void OutputHandler::DisplayHeader() {
+    out_ << center(sTitleInfo, GetWindowWidth(), ' ') << std::endl;
+    out_ << center(sCopyRight, GetWindowWidth(), ' ') << std::endl;
+}
+
+void OutputHandler::DisplayTableName(const std::string& table_name) {
+    const std::string to_display = table_name.empty() ? sDefaultTableName : table_name;
+    out_ << center(to_display, GetWindowWidth(), '=') << std::endl;
+}
+
+void OutputHandler::DisplaySeparatorBar(const std::string& name) {
+    out_ << left(name, GetWindowWidth(), '=') << std::endl;
+}
+
+void OutputHandler::DisplayError() {
+    if (error_list_.empty()) return;
+    *this << left("General Error", GetWindowWidth(), '=') << std::endl;
+    for (const std::string e_what : error_list_) {
+        *this << e_what << std::endl;
+    }
+}
+
+void OutputHandler::DisplayHint(const std::string& hint) {
+    short old_x, old_y;
+    GetCursorPos(old_x, old_y);
+    gotoxy(0, hint_zone_row_);
+    std::cout << center(hint, GetWindowWidth(), ' ');
+    gotoxy(old_x, old_y);
+}
+
+void OutputHandler::DisplayPrompt() {
+    gotoxy(0, prompt_y_);
+    out_ << left(sPrompt, GetWindowWidth(), ' ');
+    CursorBackToPrompt();
+}
+
+void OutputHandler::LogError(const std::string& e_what) {
+    error_list_.push_back(e_what);
+}
+
+void OutputHandler::CursorBackToPrompt() {
+    gotoxy(prompt_x_, prompt_y_);
+}
+
+OutputHandler& OutputHandler::operator<<(std::ostream&(* F)(std::ostream&)) { 
+    F(out_); 
+    return *this; 
+}
+
+std::string OutputHandler::center(const std::string& s, const int& w, const char delimiter) {
+    std::stringstream ss, spaces;
+    int padding = w - s.size() - 1;                 // count excess room to pad
+    for (int i = 0; i<padding / 2; ++i)
+        spaces << delimiter;
+    ss << spaces.str() << s << spaces.str();    // format with padding
+    if (padding>0 && padding % 2 != 0)               // if odd #, add 1 space
+        ss << delimiter;
+    return ss.str();
+}
+
+/*! Left-aligns string within a field of width w. Pads with blank spaces
+to enforce alignment. */
+std::string OutputHandler::left(const std::string s, const int w, const char delimiter) {
+    std::stringstream ss, spaces;
+    int padding = w - s.size() - 1;                 // count excess room to pad
+    for (int i = 0; i<padding; ++i)
+        spaces << delimiter;
+    ss << s << spaces.str();                    // format with padding
+    return ss.str();
+}
+
+
+void OutputHandler::gotoxy(const short& x, const short& y) {
+    COORD homeCoords = { x, y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), homeCoords);
+}
---
+++ lab3/OutputHandler.h
@@ -0,0 +1,100 @@
+﻿#ifndef OUTPUT_HANDLER_H
+#define OUTPUT_HANDLER_H
+#include <iostream>
+#include <sstream>
+#include <list>
+
+class OutputHandler {
+    const std::string sTitleInfo = "CSE 425 Lab 3: Resolving Horn Clause in C++";
+    const std::string sCopyRight = "By Anqi Zhang, Yu Xiao. ALL RIGHTS RESERVED";
+    const std::string sDefaultTableName = "Default Table Name";
+    const short nLinesTopUsed = 3;
+    const std::string sCommandWindow = "Command Window";
+    const std::string sPrompt = ">>";
+    const short nLinesBottomUsed = 2 + 1;  // 1 for hint zone, 1 for empty usage.
+public:
+    static short nline_user_used_;
+
+    OutputHandler(std::ostream& out);
+
+    short GetPrintableZoneHeight() const;
+
+    void DisplayHint(const std::string&);
+
+    void DisplayPrompt();
+
+    void LogError(const std::string& e_what);
+
+    OutputHandler& operator<<(std::ostream& (*F)(std::ostream&));
+    
+    // The program should use this to ouput the result, which will trigger the line logging.
+    template<typename T>
+    OutputHandler& operator<<(const T& v) {
+        LogNumLinesUsed(v);
+        out_ << v;
+        return *this;
+    }
+
+    template<typename Func>
+    void DisplayProgram(Func f, const std::string& table_name = std::string()) {
+        system("cls");
+        DisplayHeader();
+        DisplayTableName(table_name);
+        
+        // User printed predicate
+        // Should always use operator<< in this class.
+        f(GetPrintableZoneHeight());
+
+        // User Error
+        // Should list all the error occurs during operation
+        DisplayError();
+
+        if (nline_user_used_ < GetPrintableZoneHeight()) { // Some Line is not used, Move the cursor to the bottom.
+            gotoxy(0, GetWindowHeight() - nLinesBottomUsed);
+        }
+        
+        DisplaySeparatorBar(sCommandWindow);
+
+        // Log the current cursor for HintZone.
+        // Log the prompt cursor position.
+        GetCursorPos(prompt_x_, prompt_y_);
+        prompt_x_ += sPrompt.size();
+        hint_zone_row_ = prompt_y_ + 1;
+
+        nline_user_used_ = 0;  // Since the program display complete.
+        error_list_.clear();
+    }
+
+private:
+    static std::string center(const std::string& s, const int& w, const char delimiter);
+    static std::string left(const std::string s, const int w, const char delimiter);
+    static void gotoxy(const short& x, const short& y);
+    static int GetWindowSize(short& width, short& height);
+    short GetWindowWidth() const;
+    short GetWindowHeight() const;
+    static int GetCursorPos(short& x, short& y);
+    void CursorBackToPrompt();
+
+    void DisplayHeader();
+    void DisplayTableName(const std::string& = std::string());
+    void DisplaySeparatorBar(const std::string& name);
+    void DisplayError();
+
+    template<typename T>
+    void LogNumLinesUsed(const T& v) {
+        std::ostringstream oss(v);      // Use ostringstream to make sure all kind value can
+        size_t len = oss.str().size();  // convert to string, so that we can know the length
+        short window_width = GetWindowWidth();
+        nline_user_used_ += short(ceil(double(len) / window_width));
+    }
+
+    std::ostream& out_;
+
+    short hint_zone_row_;
+
+    short prompt_x_, prompt_y_;
+
+    std::list<std::string> error_list_;
+};
+
+#endif
