9a1adf20c3bf9bd3b29033006ef70e12bc67ad63
xiaoy <xiaoyuxqx@gmail.com>
2015-11-25 17:01:30 -0600

    program can correctly handle zero line display argument

--- lab3/CommandProcessor.h
+++ lab3/CommandProcessor.h
@@ -7,7 +7,7 @@
 class CommandProcessor {
 public:
     explicit CommandProcessor();
-	CommandProcessor(int);
+    CommandProcessor(int);
 
     virtual ~CommandProcessor();
 
@@ -15,34 +15,22 @@ public:
     CommandProcessor& operator= (const CommandProcessor&) = delete;
 
     void Process(const std::string& filename);
-
     void Assert(const std::string& hornclauses);
-
     void Up(const unsigned& nlines = 0);
-
     void Down(const unsigned& nlines = 0);
-
     void Resolve(const unsigned& num_first_hornclause, const unsigned& num_second_hornclause);
-
     void Randomize(std::string& variable, const unsigned& max = 0);
-
     void Set(std::string& variable, const int& value);
-
     void Print();
 
 private:
     void DisplayDatabaseEntries(const unsigned& begin);
-
     short GetDefaultDisplayNum() const;
 
     SymbolTable symbol_table_;
-
     DeductiveDatabase database_;
-
-	int display_counter_;
-
-	int display_num_;
-
+    int display_counter_;
+    int display_num_;
 };
 
 #endif
--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -13,28 +13,33 @@
 #define NUM_HORNCLAUSE_POS      1
 #define NO_ERROR                0
 
+#define PRINT_USAGE_RETURN(X) \
+    do { \
+        std::cout << "Usage: " << argv[PROGRAM_POS] << " <Number of Horn Clause> " << std::endl; \
+        return X; \
+    } while (0)
+
 int main(int argc, char** argv)
 {
     int num_hornclauses = DEFAULT_NUM_HORNCLAUSE;
     
-    if (argc > MAX_ARGS) {
-        std::cout << "Usage : " << argv[PROGRAM_POS] << " <Number of Horn Clause> " << std::endl;
-        return EXIT_FAILURE;
+    if (argc > MAX_ARGS) {  // Numbers of arguments not satisfied.
+        PRINT_USAGE_RETURN(EXIT_FAILURE);
     }
     
-    if (argc == MAX_ARGS) {
-        if (is_number(argv[NUM_HORNCLAUSE_POS]))
+    if (argc == MAX_ARGS) {  // The second should always be positive number.
+        if (is_number(argv[NUM_HORNCLAUSE_POS]) && 0 != atoi(argv[NUM_HORNCLAUSE_POS]))
             num_hornclauses = atoi(argv[NUM_HORNCLAUSE_POS]);
         else
-            std::cout << "Usage : " << argv[PROGRAM_POS] << " <Number of Honr Clause> " << std::endl;
+            PRINT_USAGE_RETURN(EXIT_FAILURE);
     }
 
-    // TODO Initialize some necessary stuffs here.
+    // Initialize some necessary stuffs here.
     CommandProcessor cmd_processor(num_hornclauses);
     InputHandler input_handler(std::cin);
 
-    // TODO Read input and make command
-    int error_code = NO_ERROR;
+    // Read input and make command
+    int error_code;
     output_handler.DisplayProgram([](const short&){});  // Print out the empty template.
     while (true) {
         try {
@@ -46,7 +51,6 @@ int main(int argc, char** argv)
         }
         catch (ProgramException& e) {
             output_handler.DisplayHint(e.what());
-            //std::cerr << e.what() << " with error code: " << e.code() << std::endl;
             // Unrecoverable error
             if (e.code() == ProgramException::kFatalError) {
                 error_code = e.code();
@@ -55,6 +59,6 @@ int main(int argc, char** argv)
                 
         }
     }
-    return error_code;;
+    return error_code;
 }
 
